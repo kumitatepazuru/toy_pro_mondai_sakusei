2130189cd908b7120c62fe0d4766f56cbf5530a7
kumitatepazuru <riku.ueda@outlook.jp>
2021-07-06 07:23:09 +0900

    markdown入力対応

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+$.fileDialog=(function(){let _clear=function(){const a=$("#"+_FILE_ID);a.off("change");if(a.length){a.remove()}};let _FILE_ID="jqueryFileDialogPlugin",_open=function(b){if(!b){throw"argument is invalid"}_clear();$("<input>").prop({type:"file",id:_FILE_ID,accept:b.accept}).css({display:"none"}).appendTo($("body"));const a=$("#"+_FILE_ID);a.trigger("click");a.on("change",function(){if(b.onChange){b.onChange(a.prop("files"))}_clear()})};return{open:_open,clear:_clear}}());
+
 $(function () {
     const welcome = new mdc.dialog.MDCDialog(document.querySelector('.welcome'));
     const license = new mdc.dialog.MDCDialog(document.querySelector('.license'));
@@ -10,6 +12,10 @@ $(function () {
     window.generate_error_dialog = new window.mdc.dialog.MDCDialog(document.querySelector("#error_generate"));
     window.loader_dialog = new window.mdc.dialog.MDCDialog(document.querySelector("#loader"));
     window.linearProgress = new window.mdc.linearProgress.MDCLinearProgress(document.querySelector('.mdc-linear-progress'));
+    const com_mde = new SimpleMDE({element: $("#com")[0], autosave: {enabled: true}, spellChecker: false});
+    const problem_mde = new SimpleMDE({element: $("#problem")[0], autosave: {enabled: true}, spellChecker: false});
+    problem_mde.value("A,Bが与えられる。\nAのB乗を求めよ。")
+    com_mde.value("AのB乗は`A**B`です。\nそれをprintするだけ!")
     loader_dialog.scrimClickAction = "";
     loader_dialog.escapeKeyAction = "";
     menu.listen("MDCMenu:selected", function (e) {
@@ -21,9 +27,9 @@ $(function () {
     ];
 
     window.random_case = [
-        [2, 3, "8",false],
-        [3, 2, "9",false],
-        [1, 1, "1",true]
+        [2, 3, "8", false],
+        [3, 2, "9", false],
+        [1, 1, "1", true]
     ];
 
     function update_case_row() {
@@ -118,7 +124,7 @@ $(function () {
             index++;
         }
         $(this).parent().parent().remove();
-    }).on("click",".random-case-change-btn",function (){
+    }).on("click", ".random-case-change-btn", function () {
         $("#random-case-dialog-title").text("ケースの変更");
         const index = $(this).parent().parent().attr('id').split("-")[1];
         $("#random-case-error").addClass("d-none");
@@ -136,7 +142,7 @@ $(function () {
         })
         $("#case_hid").text(index);
         case_dialog.open();
-    }).on("change",".case-list-checkbox",function () {
+    }).on("change", ".case-list-checkbox", function () {
         let index = $(this).parent().parent().parent().parent().attr('id').split("-")[1];
         random_case[index].pop();
         random_case[index].push($(this).prop('checked'));
@@ -201,8 +207,8 @@ $(function () {
                 html_msg += '<td class="mdc-data-table__cell"><div class="mdc-touch-target-wrapper"><div class="mdc-checkbox mdc-checkbox--touch"><input class="mdc-checkbox__native-control case-list-checkbox" type="checkbox"><div class="mdc-checkbox__background"><svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24"><path class="mdc-checkbox__checkmark-path" d="M1.73,12.91 8.1,19.28 22.79,4.59" fill="none" /></svg><div class="mdc-checkbox__mixedmark"></div></div><div class="mdc-checkbox__ripple"></div></div></div>' +
                     '<td class="mdc-data-table__cell"><button class="mdc-button mdc-button--raised w-100 random-case-change-btn"><span class="mdc-button__label">変更</span></button><td class="mdc-data-table__cell"><button class="mdc-button mdc-button--outlined w-100 random-case-remove-btn"><span class="mdc-button__ripple"></span> <span class="mdc-button__label">削除</span></button>';
                 tmp.push(false);
-                if (random_case.length > id){
-                    random_case.splice(id,1,tmp);
+                if (random_case.length > id) {
+                    random_case.splice(id, 1, tmp);
                 } else {
                     random_case.push(tmp);
                 }
@@ -218,70 +224,73 @@ $(function () {
                     new mdc.checkbox.MDCCheckbox(value);
                 })
                 case_dialog.close();
-            } catch (e){
-                console.warn("エラーを検知",e);
+            } catch (e) {
+                console.warn("エラーを検知", e);
             }
         }
     });
 
-    $("#export-json").on("click",function () {
+    $("#export-json").on("click", function () {
         const title = $("#title").val();
         const point = $("#point").val();
         const tags = $("#tags").val();
         const userid = $("#userid").val();
         const restrict = $("#restrict").val();
-        let problem = $("#problem").val();
+        let problem = problem_mde.value();
         const answer = $("#answer").val();
-        const com = $("#com").val();
+        const com = com_mde.value();
         const test_case_generator = $("#test_case_generator").val();
         const error = [];
-        if (title === ""){
+        if (title === "") {
             error.push("タイトル");
         }
-        if (point === ""){
+        if (point === "") {
             error.push("得点");
         }
-        if (userid === ""){
+        if (userid === "") {
             error.push("アカウントのID");
         }
-        if (restrict === ""){
+        if (restrict === "") {
             error.push("満たすべき制約");
         }
-        if (problem.replace(/\r?\n/g,"") === ""){
+        if (problem.replace(/\r?\n/g, "") === "") {
             error.push("問題文");
         }
-        if (answer.replace(/\r?\n/g,"") === ""){
+        if (answer.replace(/\r?\n/g, "") === "") {
             error.push("想定解");
         }
-        if (com.replace(/\r?\n/g,"") === ""){
+        if (com.replace(/\r?\n/g, "") === "") {
             error.push("解説文");
         }
-        if (test_case_generator.replace(/\r?\n/g,"") === ""){
+        if (test_case_generator.replace(/\r?\n/g, "") === "") {
             error.push("テストケース生成コード");
         }
-        if (error.length !== 0){
+        if (error.length !== 0) {
             $("#error-msg").html(error.join("<br>"));
             error_dialog.open();
         } else {
-            problem += "\n\n### 制約\n\n```python\n"+restrict+"\n```\n\n### 必要な変数\n\n"+hensuu.map(function (n){return "- "+n[0]}).join("\n")+"\n\n### 入力例\n\n```python\n"
-            hensuu.forEach(function (value,index) {
-                problem += value[0]+" = "+random_case[0][index]+"\n"
+            problem += "\n\n### 制約\n\n```python\n" + restrict + "\n```\n\n### 必要な変数\n\n" + hensuu.map(function (n) {
+                return "- " + n[0]
+            }).join("\n") + "\n\n### 入力例\n\n```python\n"
+            hensuu.forEach(function (value, index) {
+                problem += value[0] + " = " + random_case[0][index] + "\n"
             })
-            problem += "```\n\n### 出力例\n\n```python\n"+random_case[0].slice(-2)[0]+"\n```"
-            const j = {"title":title,
-                "rating":Number(point),
-                "tags":tags,
-                "user_id":userid,
-                "restrict":restrict,
-                "question":problem,
-                "test_case":{
-                    "variables":{},
-                    "cases":[],
-                    "corner_cases":[]
+            problem += "```\n\n### 出力例\n\n```python\n" + random_case[0].slice(-2)[0] + "\n```"
+            const j = {
+                "title": title,
+                "rating": Number(point),
+                "tags": tags,
+                "user_id": userid,
+                "restrict": restrict,
+                "question": problem,
+                "test_case": {
+                    "variables": {},
+                    "cases": [],
+                    "corner_cases": []
                 },
-                "expected_answer":answer,
-                "test_case_generator":test_case_generator,
-                "comment":com
+                "expected_answer": answer,
+                "test_case_generator": test_case_generator,
+                "comment": com
             }
             hensuu.forEach(function (value) {
                 j.test_case.variables[value[0]] = value[1];
@@ -291,7 +300,7 @@ $(function () {
                 hensuu.forEach(function (i, index, array) {
                     tmp[i[0]] = value[index];
                 });
-                if (value.slice(-1)[0]){
+                if (value.slice(-1)[0]) {
                     j.test_case.corner_cases.push({
                         "inputs": tmp,
                         "output": value.slice(-2)[0]
@@ -305,17 +314,53 @@ $(function () {
             })
 
             console.log(j);
-            $("#code").text(JSON.stringify(j,null,"  "));
+            $("#code").text(JSON.stringify(j, null, "  "));
             success_dialog.open();
         }
     });
 
-    $("#save-json").on("click",function(){
+    $("#save-json").on("click", function () {
         $('<a>', {
             href: 'data:text/plain,' + encodeURIComponent($("#code").text()),
-            download: $("#title").val()+".json"
+            download: $("#title").val() + ".json"
         })[0].click();
     });
+
+    $("#import-json").on("click", function () {
+        $.fileDialog.open({
+            accept: '.json',
+            onChange: function(files) {
+                const file = files[0];
+                const reader = new FileReader();
+                reader.onload = () => {
+                    const data = JSON.parse(reader.result);
+                    $("#title").val(data.title);
+                    $("#point").val(data.rating);
+                    $("#tags").val(data.tags);
+                    $("#userid").val(data.user_id);
+                    $("#restrict").val(data.restrict);
+                    problem_mde.value(data.question);
+                    let index = 0;
+                    while ($("#hensuu-" + index).length) {
+                        $("#hensuu-" + index).remove();
+                        index++;
+                    }
+                    window.hensuu = [];
+                    Object.keys(data.test_case.variables).forEach(function (key){
+                        $("#hensuu-list").append('<tr class="mdc-data-table__row" id="hensuu-' + hensuu.length + '"><th class="mdc-data-table__cell name">'+key+'</th><td class="mdc-data-table__cell hname">'+data.test_case.variables[key]+'</td><td class="mdc-data-table__cell"><button class="mdc-button mdc-button--raised w-100 change-btn"><span class="mdc-button__label">変更</span></button></td><td class="mdc-data-table__cell"><button class="mdc-button mdc-button--outlined w-100 remove-btn"><span class="mdc-button__ripple"></span><span class="mdc-button__label">削除</span></button></td></tr>')
+                        document.querySelectorAll('#hensuu-list .mdc-button').forEach(function (value) {
+                            new mdc.ripple.MDCRipple(value);
+                        })
+                        hensuu.push([key,data.test_case.variables[key]]);
+                    });
+
+                    update_case_row();
+                };
+
+                reader.readAsText(file);
+            }
+        });
+    })
 
     brython(1);
 });
